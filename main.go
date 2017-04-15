7c7729c337758e2bd6c89c4702cf67cd8e86c701
Sung J Byun <sungjbyun@Sungs-MacBook-Pro-2.local>
2017-04-15 13:58:14 -0700

    mux deprecated

--- main.go
+++ main.go
@@ -1,38 +1,35 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
 	_"github.com/julienschmidt/httprouter"
+	"html/template"
 	"log"
+	"net/http"
 )
 
 var tpl *template.Template
 
 func init() {
-	// tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
 	tpl = template.Must(template.ParseGlob("templates/*"))
-
 }
 
 func main() {
+	// mux := httprouter.New()
+	// mux.GET("/", index)
+	// mux.GET("/contact", contact)
 	http.HandleFunc("/", index)
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
 	http.HandleFunc("/contact", contact)
 	http.ListenAndServe(":8080", nil)
-	// mux := httprouter.New()
-	// mux.GET("/", index)
-	// http.ListenAndServe(":8080", mux)
 }
 
-func contact(w http.ResponseWriter, _ *http.Request) {
-	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
+func index(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
 	HandleError(w, err)
 }
 
-func index(w http.ResponseWriter, _ *http.Request) {
-	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
-	// tpl.ExecuteTemplate(w, "index.gohtml", nil)
+func contact(w http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
 	HandleError(w, err)
 }
 
