baeb29df988eb7b4e62f75369bcf34f1bd01270d
Sung J Byun <sungjbyun@Sungs-MacBook-Pro-2.local>
2017-04-15 13:21:18 -0700

    sync with master branch

--- main.go
+++ main.go
@@ -3,21 +3,42 @@ package main
 import (
 	"html/template"
 	"net/http"
+	_"github.com/julienschmidt/httprouter"
+	"log"
 )
 
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+	// tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+	tpl = template.Must(template.ParseGlob("templates/*"))
+
 }
 
 func main() {
 	http.HandleFunc("/", index)
-	http.HandleFunc("contact", contact)
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
+	http.HandleFunc("/contact", contact)
 	http.ListenAndServe(":8080", nil)
+	// mux := httprouter.New()
+	// mux.GET("/", index)
+	// http.ListenAndServe(":8080", mux)
+}
+
+func contact(w http.ResponseWriter, _ *http.Request) {
+	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
+	HandleError(w, err)
 }
 
 func index(w http.ResponseWriter, _ *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	// tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	HandleError(w, err)
+}
+
+func HandleError(w http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalln(err)
+	}
 }
